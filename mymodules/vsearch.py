236e5b7e7b8392d56da9e83b7d95c868419aeefd
andriell <root.andriell@gmail.com>
2018-12-11 15:46:40 +0300

    tensorflow

---
+++ mymodules/vsearch.py
@@ -0,0 +1,4 @@
+
+def search_for_letters(phrase:str, letters:str = 'aeiou') -> set:
+    """Возвращает множество букв из 'letters', найденных в указанной фразе."""
+    return set(letters).intersection(set(phrase))

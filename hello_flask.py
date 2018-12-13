843aad8d634a9d39176288df9c85bb492f4e1d01
andriell <root.andriell@gmail.com>
2018-12-13 15:45:36 +0300

    tensorflow

---
+++ hello_flask.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request, redirect
+import vsearch
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello() -> '302':
+    return redirect('/entry')
+
+
+@app.route('/search', methods=['POST'])
+def search() -> 'html':
+    title = 'title = Here are your results:'
+    phrase = request.form['phrase']
+    letters = request.form['letters']
+    results = vsearch.search_for_letters(phrase, letters)
+    return render_template('results.html',
+                           the_phrase=phrase,
+                           the_letters=letters,
+                           the_title=title,
+                           the_results=results,
+                           )
+
+@app.route('/entry')
+def entry() -> 'html':
+    return render_template('entry.html', the_title='Welcome to search4letters on the web!')
+
+
+app.run(debug=True)

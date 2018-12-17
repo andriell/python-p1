85cd64854143e8be3f47570a3c15940c13491970
andriell <root.andriell@gmail.com>
2018-12-17 16:59:04 +0300

    tensorflow

--- webapp/vsearch4web.py
+++ webapp/vsearch4web.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template, request, redirect, send_from_directory, escape
+from flask import Flask, render_template, request, redirect, send_from_directory, escape, session
 import os
 import vsearch
 from webapp import DBcm
 
 app = Flask(__name__)
 
+app.secret_key = 'YouWillNeverGuess'
 app.config['dbconfig'] = {'host': '127.0.0.1',
                           'port': '3316',
                           'user': 'vsearch',
@@ -69,6 +70,36 @@ def view_the_log() -> str:
                            the_title='View Log',
                            the_row_titles=titles,
                            the_data=contents, )
+
+
+@app.route('/setuser/<user>')
+def set_user(user: str) -> str:
+    session['user'] = user
+    return 'User value set to: ' + session['user']
+
+
+@app.route('/getuser')
+def get_user() -> str:
+    return 'User value is currently set to: ' + session['user']
+
+
+@app.route('/login')
+def do_login() -> str:
+    session['logged_in'] = True
+    return 'You are now logged in'
+
+
+@app.route('/logout')
+def do_logout() -> str:
+    session.pop('logged_in')
+    return 'You are now logged out'
+
+
+@app.route('/status')
+def check_status() -> str:
+    if 'logged_in' in session:
+        return 'You are currently logged in'
+    return 'You are NOT logged in'
 
 
 if __name__ == '__main__':

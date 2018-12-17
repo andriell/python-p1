8791031a80cecb3b2eed94a9017e4aa600e4e976
andriell <root.andriell@gmail.com>
2018-12-17 15:53:44 +0300

    tensorflow

--- webapp/vsearch4web.py
+++ webapp/vsearch4web.py
@@ -1,13 +1,31 @@
 from flask import Flask, render_template, request, redirect, send_from_directory, escape
 import os
 import vsearch
+import mysql.connector
 
 app = Flask(__name__)
 
 
 def log_request(req: 'flask_request', res: str) -> None:
-    with open('vsearch.log', 'a') as log:
-        print(req.form, req.remote_addr, req.user_agent, res, file=log, sep='|')
+    dbconfig = {'host': '127.0.0.1',
+                'port': '3316',
+                'user': 'vsearch',
+                'password': 'vsearchpasswd',
+                'database': 'vsearchlogDB', }
+    conn = mysql.connector.connect(**dbconfig)
+    cursor = conn.cursor()
+    _SQL = """insert into log
+    (phrase, letters, ip, browser_string, results)
+    values
+    (%s, %s, %s, %s, %s)"""
+    cursor.execute(_SQL, (req.form['phrase'],
+                          req.form['letters'],
+                          req.remote_addr,
+                          req.user_agent.browser,
+                          res,))
+    conn.commit()
+    cursor.close()
+    conn.close()
 
 
 @app.route('/favicon.ico')

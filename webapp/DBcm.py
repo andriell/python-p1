cb50604099c4573b5deb73bf7d7420b188bce461
andriell <root.andriell@gmail.com>
2018-12-19 18:42:57 +0300

    tensorflow

--- webapp/DBcm.py
+++ webapp/DBcm.py
@@ -2,17 +2,30 @@ import mysql.connector
 from mysql.connector import cursor
 
 
+class ConnectionError(Exception):
+    pass
+
+
+class SQLError(Exception):
+    pass
+
+
 class UseDatabase:
 
     def __init__(self, config: dict) -> None:
         self.configuration = config
 
     def __enter__(self) -> 'cursor':
-        self.conn = mysql.connector.connect(**self.configuration)
-        self.cursor = self.conn.cursor()
-        return self.cursor
+        try:
+            self.conn = mysql.connector.connect(**self.configuration)
+            self.cursor = self.conn.cursor()
+            return self.cursor
+        except Exception as error:
+            raise ConnectionError(error)
 
     def __exit__(self, exc_type, exc_val, exc_tb) -> None:
         self.conn.commit()
         self.cursor.close()
         self.conn.close()
+        if exc_type is mysql.connector.errors.ProgrammingError:
+            raise SQLError(exc_val)
